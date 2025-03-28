2cf355ee79b92bd327edd6ea88a38ccde640d80f
chinhtrieudong <lebachinh2002@gmail.com>
2025-03-28 15:15:08 +0700

    generate and verify jwt

---
+++ src/main/java/com/example/identify_service/controller/AuthenticationController.java
@@ -0,0 +1,39 @@
+package com.example.identify_service.controller;
+
+import com.example.identify_service.dto.request.ApiResponse;
+import com.example.identify_service.dto.request.AuthenticationRequest;
+import com.example.identify_service.dto.request.IntrospectRequest;
+import com.example.identify_service.dto.response.AuthenticationResponse;
+import com.example.identify_service.dto.response.IntrospectResponse;
+import com.example.identify_service.service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationController {
+    AuthenticationService authenticationService;
+
+    @PostMapping("/token")
+    public ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        var result = authenticationService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>builder().result(result).build();
+    }
+
+
+    @PostMapping("/introspect")
+    public ApiResponse<IntrospectResponse> verify(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+        var result = authenticationService.introspect(request);
+        return ApiResponse.<IntrospectResponse>builder().result(result).build();
+    }
+}
--- src/main/java/com/example/identify_service/service/UserService.java
+++ src/main/java/com/example/identify_service/service/UserService.java
@@ -11,6 +11,8 @@ import com.example.identify_service.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -38,6 +40,8 @@ public class UserService {
             throw new AppException(ErrorCode.USER_EXISTED);
         }
         User user = userMapper.toUser(request);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
         return userMapper.toUserReponse(userRepository.save(user));
     }
 
@@ -45,6 +49,8 @@ public class UserService {
         User user = userRepository.findById(userId).orElseThrow(
                 () -> new RuntimeException("User not found")
         );
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        request.setPassword(passwordEncoder.encode(request.getPassword()));
         userMapper.updateUser(user, request);
         return userMapper.toUserReponse(userRepository.save(user));
     }
